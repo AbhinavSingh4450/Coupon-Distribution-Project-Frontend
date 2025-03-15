99178c32c230d49af546b911e0d465312ecbd5ae
Abhinav Singh <sabhinavpratap2@gmail.com>
2025-03-16 00:26:17 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // Default Vite port
-  },
-  build: {
-    outDir: 'dist', // Ensure build files go into the correct folder
-  },
-  base: '/', // This ensures correct routing after deployment
-});
+})
